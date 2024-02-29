db3ef0ba0201b5abae8290a068515ae2f9363986
liangxiupeng <yi553852977pin@163.com>
2024-02-29 15:45:20 +0800

    日志数据转发采集服务

---
+++ gmall-parent/gmall-logger/src/main/java/com/gmall/GmallLoggerApplication.java
@@ -0,0 +1,15 @@
+package com.gmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//SpringBoot程序执行的入口类
+//当SpringBoot程序执行的时候，会扫描同级别包以及子包下的所有标记类，交给Spring进行管理
+@SpringBootApplication
+public class GmallLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallLoggerApplication.class, args);
+    }
+
+}
